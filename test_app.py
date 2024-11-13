5e6ce617a312bc4e90173bc230e8d9b265efdd9e
Ankit240603 <111530169+Ankit240603@users.noreply.github.com>
2024-11-13 13:12:10 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,8 +3,8 @@ import unittest
 
 class TestApp(unittest.TestCase):
     def test_add(self):
-	return self.assertEqual(add(5,3),8,"Should be 8")
+      return self.assertEqual(add(5,3),8,"Should be 8")
     def test_sub(self):
-	return self.assertEqual(add(5,3),2,"Should be 2")
+      return self.assertEqual(add(5,3),2,"Should be 2")
 
 unittest.main()
